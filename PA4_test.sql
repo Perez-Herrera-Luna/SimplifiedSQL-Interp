fc06eb07d2fd0c4becdeb4c014c15170e65cffc1
Luna Perez-Herrera <lunaperezherrera@gmail.com>
2026-06-26 15:18:25 -0700

    Modify PA4_test.sql to adhere to syntax

--- PA4_test.sql
+++ PA4_test.sql
@@ -1,30 +1,16 @@
 -- CS 457/657 PA4
 
--- This script includes the commands to be executed by two processes (or terminals), P1 and P2
-
 -- On P1:
 CREATE DATABASE CS457_PA4;
 USE CS457_PA4;
 create table Flights (seat int, status int);
-insert into Flights values(22, 0); -- seat 22 is available
-insert into Flights values(23, 1); -- seat 23 is occupied
+insert into Flights values(22, 0);
+insert into Flights values(23, 1);
 begin transaction;
 update flights set status = 1 where seat = 22;
-
--- On P2:
-USE CS457_PA4;
+commit;
 select * from Flights;
-begin transaction;
-update flights set status = 1 where seat = 22;
-commit; --there should be nothing to commit; it's an "abort"
-select * from Flights;
-
--- On P1:
-commit; --persist the change to disk
-select * from Flights;
-
--- On P2:
-select * from Flights;
+.exit
 
 ---------------------
 -- Expected output --
@@ -42,19 +28,4 @@ select * from Flights;
 -- seat int|status int
 -- 22|1
 -- 23|1
-
-
--- On P2:
--- Using database CS457_PA4.
--- seat int|status int
--- 22|0
--- 23|1
--- Transaction starts.
--- Error: Table Flights is locked!
--- Transaction abort.
--- seat int|status int
--- 22|0
--- 23|1
--- seat int|status int
--- 22|1
--- 23|1
+-- All done.
